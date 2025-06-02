a305fae3fab0c0c570ec55fbf793090ac0326bc5
jtarvain <jtarvain-hive@gmail.com>
2025-06-02 12:55:54 +0300

    ready for testing on site

--- ft_printf.c
+++ ft_printf.c
@@ -18,26 +18,21 @@ int	ft_printf(const char *str, ...)
 	int		count;
 	va_list	args;
 
-	if (!*str)
-		return (0);
-	else
+	count = 0;
+	va_start(args, str);
+	while (*str)
 	{
-		count = 0;
-		va_start(args, str);
-		while (*str)
+		if (*str != '%')
 		{
-			if (*str != '%')
-			{
-				ft_putchar_fd(*str++, 1);
-				count++;
-			}
-			else
-			{
-				str++;
-				parse_specifier(&str, &count, args);
-			}
+			ft_putchar_fd(*str++, 1);
+			count++;
 		}
-		va_end(args);
+		else
+		{
+			str++;
+			parse_specifier(&str, &count, args);
+		}
 	}
+	va_end(args);
 	return (count);
 }
